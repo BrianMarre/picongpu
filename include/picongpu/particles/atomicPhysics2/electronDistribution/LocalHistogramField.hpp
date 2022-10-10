a8eb8fc12a01a0c462374abb95b0be63a63f2c10
BrianMarre <b.marre@hzdr.de>
2022-10-10 16:30:18 +0000

    switch LocalHistogramField to generalized SuperCellField

--- include/picongpu/particles/atomicPhysics2/electronDistribution/LocalHistogramField.hpp
+++ include/picongpu/particles/atomicPhysics2/electronDistribution/LocalHistogramField.hpp
@@ -23,11 +23,7 @@
 
 #pragma once
 
-#include <pmacc/dataManagement/ISimulationData.hpp>
-#include <pmacc/dimensions/DataSpace.hpp>
-#include <pmacc/fields/SimulationFieldHelper.hpp>
-#include <pmacc/memory/boxes/PitchedBox.hpp>
-#include <pmacc/memory/buffers/GridBuffer.hpp>
+#include "picongpu/particles/atomicPhysics2/SuperCellField.hpp"
 
 #include <cstdint>
 #include <string>
@@ -42,41 +38,20 @@ namespace picongpu
             {
                 /**@class holds a gridBuffer of the per-superCell localHistograms for atomicPhysics
                  *
+                 * @attention histograms are uninitialized upon creation of the field,
+                 *  use .getDeviceBuffer().setValue() to init
                  */
                 template<typename T_Histogram, typename T_MappingDescription>
-                struct LocalHistogramField
-                    : public pmacc::ISimulationData
-                    , public pmacc::SimulationFieldHelper<T_MappingDescription>
+                struct LocalHistogramField : SuperCellField<T_Histogram, T_MappingDescription>
                 {
-                    //! type of data box for field values on host and device
-                    using DataBoxType = pmacc::DataBox<pmacc::PitchedBox<T_Histogram, simDim>>;
-                    //! type of device buffer
-                    using DeviceBufferType = pmacc::DeviceBuffer<T_Histogram, simDim>;
-
-                    /* from SimulationFieldHelper<T_MappingDescription>:
-                     * protected:
-                     *      T_MappingDescription cellDescription;
-                     * public:
-                     *      static constexpr uint32_t dim = T_MappingDescription::dim;
-                     *      using MappingDesc = T_MappingDescription;
-                     *      void ~<> = default;
-                     *
-                     *      T_MappingDescription getCellDescription() const
-                     */
-
-                    /// @todo should be private?
-                    //! pointer to gridBuffer of histograms:T_histograms created upon creation
-                    std::unique_ptr<pmacc::GridBuffer<T_Histogram, simDim>> localHistogramField;
                     /// @todo should these be private?
                     //! type of physical particle represented in histogram, usually "Electron" or "Photon"
                     std::string histogramType;
 
                     LocalHistogramField(T_MappingDescription const& mappingDesc, std::string const histogramType)
-                        : SimulationFieldHelper<T_MappingDescription>(mappingDesc)
+                        : SuperCellField<T_Histogram, T_MappingDescription>(mappingDesc)
                         , histogramType(histogramType)
                     {
-                        this->localHistogramField
-                            = std::make_unique<GridBuffer<T_Histogram, simDim>>(mappingDesc.getGridSuperCells());
                     }
 
                     // required by ISimulationData
@@ -85,41 +60,6 @@ namespace picongpu
                         return histogramType + "_localHistogramField";
                     }
 
-                    // required by ISimulationData
-                    //! == deviceToHost
-                    void synchronize() override
-                    {
-                        localHistogramField->deviceToHost();
-                    }
-
-                    // required by SimulationFieldHelper
-                    void reset(uint32_t currentStep) override
-                    {
-                         /// @todo figure out why exactly this way
-                         localHistogramField->getHostBuffer().reset(true);
-                         localHistogramField->getDeviceBuffer().reset(false);
-                    };
-
-                    // required by SimulationHelperField
-                    //! ==hostToDevice
-                    void syncToDevice() override
-                    {
-                        localHistogramField->hostToDevice();
-                    }
-
-                    /** get dataBox on device for use in device kernels
-                     *
-                     * Note: dataBoxes are just "pointers"
-                     */
-                    DataBoxType getDeviceDataBox()
-                    {
-                        return this->localHistogramField->getDeviceBuffer().getDataBox();
-                    }
-
-                    DeviceBufferType& getDeviceBuffer()
-                    {
-                        return this->localHistogramField->getDeviceBuffer();
-                    }
                 };
             } // namespace electronHistogram
         } // namespace atomicPhysics2
