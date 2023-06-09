d7920776e5d72029ab346b2ad9ed81f326c4cadb
BrianMarre <b.marre@hzdr.de>
2023-06-09 15:51:02 +0200

    upadte to new charge state manipulator

--- include/picongpu/particles/atomicPhysics2/SetToAtomicGroundStateForChargeState.hpp
+++ include/picongpu/particles/atomicPhysics2/SetToAtomicGroundStateForChargeState.hpp
@@ -21,78 +21,66 @@
 
 #include <pmacc/traits/HasFlag.hpp>
 
-namespace picongpu
+namespace picongpu::particles::atomciPhysics2
 {
-    namespace particles
+    struct SetToAtomicGroundStateForChargeState
     {
-        namespace atomicPhysics2
+        /** set a given ion to its ground state for a given number of bound
+         *  electrons
+         *
+         *  Functor, sets a given ion's boundElectrons attribute,
+         *  and, if present, atomicState attribute, to the correct value/s
+         *  for its ground state.
+         *
+         *  Example:
+         *    Ar(Z=18), boundElectrons=3 --> super configuration(2,1,0,0,...)
+         *
+         *  BEWARE: Different Usage from previous SetIonization(),
+         *      specify NUMBER of bound electrons NOT charge state.
+         *  BEWARE: Uses simple fill from bottom shell wise for ground state
+         *      determination, not actually entire truth.
+         *  @todo : implement full madelung occupation schema, and exceptions,
+         *      like Cu.
+         */
+        template<typename T_Ion>
+        DINLINE void operator()(T_Ion& ion, float_X numberBoundElectrons)
         {
-            struct SetToAtomicGroundStateForChargeState
+            PMACC_DEVICE_ASSERT_MSG(numberBoundElectrons >= 0, "Number of bound electrons must be >= 0");
+
+            // init atomic state consistently if present
+            if constexpr(pmacc::traits::HasFlag<typename T_Ion::FrameType, isAtomicPhysicsIon<>>::type::
+                                value)
             {
-                template<typename T_Particle>
-                DINLINE void operator()(T_Particle& particle, float_X numberBoundElectrons)
+                // get current Configuration number object
+                auto configNumber = ion[atomicConfigNumber_];
+
+                // create blank occupation number vector
+                auto occupationNumberVector
+                    = pmacc::math::Vector<uint8_t, configNumber.numberLevels>::create(0);
+                // could actually be reduced to uint8_t since Z<=98(Californium) for our purposes
+
+                uint8_t numberElectronsRemaining = static_cast<uint8_t>(numberBoundElectrons);
+
+                // fill from bottom up until no electrons remaining -> ground state init
+                /// @todo : implement Mandelung Schema and exceptions, Brian Marre, 2022
+                for(uint8_t level = 1u; level <= configNumber.numberLevels; level++)
                 {
-                    /** set a given ion to its ground state for a given number of bound
-                     *  electrons
-                     *
-                     *  Functor, sets a given particle's boundElectrons attribute,
-                     *  and, if present, atomicState attribute, to the correct value/s
-                     *  for its ground state.
-                     *
-                     *  Example:
-                     *    Ar(Z=18), boundElectrons=3 --> super configuration(2,1,0,0,...)
-                     *
-                     *  BEWARE: Different Usage from previous SetIonization(),
-                     *      specify NUMBER of bound electrons NOT charge state.
-                     *  BEWARE: Uses simple fill from bottom shell wise for ground state
-                     *      determination, not actually entire truth.
-                     *  @todo : implement full madelung occupation schema, and exceptions,
-                     *      like Cu.
-                     */
-
-                    PMACC_DEVICE_ASSERT_MSG(numberBoundElectrons >= 0, "Number of bound electrons must be >= 0");
-
-                    using Particle = T_Particle;
-
-                    particle[boundElectrons_] = numberBoundElectrons;
-
-                    // init atomic state consistently if present
-                    if constexpr(pmacc::traits::HasFlag<typename T_Particle::FrameType, isAtomicPhysicsIon<>>::type::
-                                     value)
+                    // g(n) = 2*n^2; for hydrogen like states
+                    if(numberElectronsRemaining >= 2u * level * level)
                     {
-                        // get current Configuration number object
-                        auto configNumber = particle[atomicConfigNumber_];
-
-                        // create blank occupation number vector
-                        auto occupationNumberVector
-                            = pmacc::math::Vector<uint8_t, configNumber.numberLevels>::create(0);
-                        // could actually be reduced to uint8_t since Z<=98(Californium) for our purposes
-
-                        uint8_t numberElectronsRemaining = static_cast<uint8_t>(numberBoundElectrons);
-
-                        // fill from bottom up until no electrons remaining -> ground state init
-                        /// @todo : implement Mandelung Schema and exceptions, Brian Marre, 2022
-                        for(uint8_t level = 1u; level <= configNumber.numberLevels; level++)
-                        {
-                            // g(n) = 2*n^2; for hydrogen like states
-                            if(numberElectronsRemaining >= 2u * level * level)
-                            {
-                                (occupationNumberVector)[level - 1u] = 2u * level * level;
-                                numberElectronsRemaining -= 2u * level * level;
-                            }
-                            else
-                            {
-                                (occupationNumberVector)[level - 1u] = numberElectronsRemaining;
-                                break;
-                            }
-                        }
-
-                        // set atomic state index
-                        particle[atomicConfigNumber_] = configNumber.getAtomicConfigNumber(occupationNumberVector);
+                        (occupationNumberVector)[level - 1u] = 2u * level * level;
+                        numberElectronsRemaining -= 2u * level * level;
+                    }
+                    else
+                    {
+                        (occupationNumberVector)[level - 1u] = numberElectronsRemaining;
+                        break;
                     }
                 }
-            };
 
-        } // namespace atomicPhysics2
-    } // namespace particles
-} // namespace picongpu
+                // set atomic state index
+                ion[atomicConfigNumber_] = configNumber.getAtomicConfigNumber(occupationNumberVector);
+            }
+        }
+    };
+} // namespace picongpu::particles::atomicPhysics2
