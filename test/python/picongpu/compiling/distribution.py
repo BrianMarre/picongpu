1a9220f724ec390eb162edbfd6ffc07c62861956
BrianMarre <b.marre@hzdr.de>
2024-07-04 14:26:53 +0200

    add compile test for foilDistribution

--- test/python/picongpu/compiling/distribution.py
+++ test/python/picongpu/compiling/distribution.py
@@ -59,6 +59,44 @@ class TestDistribution(unittest.TestCase):
         )
         self._compile_distribution(gaussian_dist)
 
+    def test_foil_pre_and_post(self):
+        foil_dist = picmi.FoilDistribution(
+            density=8.0e24,
+            front=2.0e-5,
+            thickness=1.0e-5,
+            exponential_pre_plasma_length=1.0e-6,
+            exponential_pre_plasma_cutoff=1.0e-5,
+            exponential_post_plasma_length=1.0e-6,
+            exponential_post_plasma_cutoff=1.0e-5,
+        )
+        self._compile_distribution(foil_dist)
+
+    def test_foil_pre(self):
+        foil_dist = picmi.FoilDistribution(
+            density=8.0e24,
+            front=2.0e-5,
+            thickness=1.0e-5,
+            exponential_pre_plasma_length=1.0e-6,
+            exponential_pre_plasma_cutoff=1.0e-5,
+        )
+        self._compile_distribution(foil_dist)
+
+    def test_foil_post(self):
+        # with post-plasma only
+        foil_dist = picmi.FoilDistribution(
+            density=8.0e24,
+            front=2.0e-5,
+            thickness=1.0e-5,
+            exponential_post_plasma_length=1.0e-6,
+            exponential_post_plasma_cutoff=1.0e-5,
+        )
+        self._compile_distribution(foil_dist)
+
+    def test_foil_nothing(self):
+        # with no pre- or post-plasma
+        foil_dist = picmi.FoilDistribution(density=8.0e24, front=2.0e-5, thickness=1.0e-5)
+        self._compile_distribution(foil_dist)
+
     # tests for analytic distribution and gaussian-bunch distribution have been
     #   removed for now, see issue #4367 for the test cases
 
